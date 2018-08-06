461ddc1cc5ff92514c9677748166f4382d6d7a24
dorion <dorionka@freemail.hu>
2018-08-06 08:39:16 +0200

    Add int to bin

---
+++ 1.7.binarisba/1.7.binarisba/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1._7.binarisba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szám ami binárisba lesz kiírva:");
+            var number = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(number, 2));
+        }
+    }
+}
