6337920a78f3262bbd8f7cdd76e6dec62a7453b1
dorion <dorionka@freemail.hu>
2018-08-10 13:26:28 +0200

    Add stopwatch sulution

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StopWatch
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var Stopper = new StopWatch();
+                Stopper.Start();
+                System.Threading.Thread.Sleep(1000);
+                Stopper.Stop();
+                Console.WriteLine(Stopper.Interval.Seconds);
+
+                Stopper.Start();
+                System.Threading.Thread.Sleep(1200);
+                Stopper.Stop();
+                Console.WriteLine(Stopper.Interval.Seconds);
+
+                Stopper.Start();
+                Stopper.Start();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ StopWatch/StopWatch/StopWatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class StopWatch
+    {
+        private DateTime StartTime { get; set; } 
+        private DateTime StopTime { get; set; }
+        private bool IsRunning { get; set; }
+        public TimeSpan Interval { get; set; }
+
+        public StopWatch() {
+            IsRunning = false;    
+        }
+
+        public void Start() {
+            if (IsRunning)
+                throw new System.InvalidOperationException("You already started the stopper!");
+            
+            StartTime = DateTime.Now;
+            Interval = TimeSpan.Zero;
+            IsRunning = true;
+        }
+
+        public void Stop() {
+            StopTime = DateTime.Now;
+            Interval =  StopTime - StartTime;
+            IsRunning = false;
+        }
+    }
+}
